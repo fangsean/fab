89a1f7fca21768e342399435eede460a4dff6194
jsen.yin <jsen.yin@gmail.com>
2018-08-25 10:47:03 +0800

    addr

--- release/demo1.py
+++ release/demo1.py
@@ -1,62 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+
+import confirm
 from fabric.api import *
 from fabric.colors import *
-
-# from utils.pathfile import gitpath, branch, git1, buildpath1, local_path1, appliation1, user, password, hosts1
-from fabric.context_managers import *
-
-import configparser
-
-# config
-conf = configparser.ConfigParser()
-conf.read("/root/work/fab/properties/path.properties", encoding='utf-8')
-conf.sections()
-
-# user and passwd
-user = conf.get('Account', 'user')
-password = conf.get('Account', 'password')
-
-# host
-hosts1 = conf.get('server', 'hosts1')
-hosts2 = conf.get('server', 'hosts2')
-hosts3 = conf.get('server', 'hosts3')
-hosts4 = conf.get('server', 'hosts4')
-hosts5 = conf.get('server', 'hosts5')
-
-# branch
-branch = conf.get('branch', 'branch')
-
-# git_clone_address
-git1 = conf.get('gitpath', 'git1')
-git2 = conf.get('gitpath', 'git2')
-git3 = conf.get('gitpath', 'git3')
-git4 = conf.get('gitpath', 'git4')
-
-# git_storage_address
-gitpath = conf.get('path', 'gitpath')
-
-# build_path
-buildpath1 = conf.get('path', 'buildpath1')
-buildpath2 = conf.get('path', 'buildpath2')
-buildpath3 = conf.get('path', 'buildpath3')
-buildpath4 = conf.get('path', 'buildpath4')
-
-# war_pak_path
-local_path1 = conf.get('path', 'local_path1')
-local_path2 = conf.get('path', 'local_path2')
-local_path3 = conf.get('path', 'local_path3')
-local_path4 = conf.get('path', 'local_path4')
-
-# release_path
-appliation1 = conf.get('path', 'appliation1')
-appliation2 = conf.get('path', 'appliation2')
-appliation3 = conf.get('path', 'appliation3')
+from utils.pathfile import gitpath, branch, git1, user, password, hosts1, buildpath1, appliation1, local_path1
 
 env.user = user
 env.password = password
 env.hosts = hosts1
-
 
 @runs_once
 def demo_mvn_clone():
@@ -106,8 +58,12 @@ def demo_put_before():
 def demo_put():
     print("[INFO]  ............................................ 远程发包 > demo_put")
     with lcd(local_path1):
-        put('bsweb.jar', appliation1)
-    print("[INFO]  ............................................ 远程发包成功 > demo_put")
+        result = put('bsweb.jar', appliation1 + 'bsweb.jar')
+        if result.failed and not confirm("put file faild, Continue[Y/N]?"):
+            abort("Aborting file put task!")
+            print("[INFO]  ............................................ 远程发包失败 > demo_put")
+        else:
+            print("[INFO]  ............................................ 远程发包成功 > demo_put")
 
 
 @runs_once
--- utils/pathfile.py
+++ utils/pathfile.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-# name : Alenx
 import configparser
 
 # config
